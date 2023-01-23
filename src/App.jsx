c5b1e2c609dc0fca9acc6a163ba4fc1c68f1463c
moshecc <moshec545@gmail.com>
2023-01-23 14:54:47 +0200

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Card from './components/Card/Card'
 import HomePage from './components/HomePage/HomePage'
 
 export default function App() {
   return (
     <>
-    <HomePage/>
+    {/* <HomePage/> */}
+    <Card/>
     </>
   )
 }
