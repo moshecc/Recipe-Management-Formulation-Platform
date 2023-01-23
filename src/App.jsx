3589843575dd82219d3e349f2bbbfc6d366e7ef3
moshecc <moshec545@gmail.com>
2023-01-23 21:47:45 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,9 @@ import HomePage from './components/HomePage/HomePage'
 export default function App() {
   return (
     <>
-<<<<<<< HEAD
-    <HomePage/>
-=======
     {/* <HomePage/> */}
-    <NewCard/>
-    <Card/>
->>>>>>> 41752d04e44a7103c3db4f718a483830ce263323
+    {/* <NewCard/> */}
+    {/* <Card/> */}
     </>
   )
 }
