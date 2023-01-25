890094b04351b9537bc58ee72bcb2bf1cfb6bd23
haim hale <hayim9939@gmail.com>
2023-01-25 08:47:29 +0200

    card perfect

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,13 +7,13 @@ export default function Card() {
     <Link to={""}>
     <div className="card text-dark m-3 p-1 cardHover ">
           <img className='card-img imgStyle' width={150} height={150} src="https://i.imagesup.co/images2/4c7cc05dd420f94ff35456056d5a114499bbbb62.jpg" alt="card" />
-         <div class="card-img-overlay">
-           <h3 class="card-title">Card title</h3>
-           <p class="card-text">Last updated 3 mins ago</p>
+         <div className="card-img-overlay">
+           <h3 className="card-title">Card title</h3>
+           <p className="card-text">Last updated 3 mins ago</p>
            <div>❤️</div>
-           <i class="fa-solid fa-heart"></i>
-           <i class="fa-regular fa-heart"></i>
-           <i class="fa-solid fa-share-nodes ml-5"></i>          
+           <i className="fa-solid fa-heart"></i>
+           <i className="fa-regular fa-heart"></i>
+           <i className="fa-solid fa-share-nodes ml-5"></i>          
           </div>
     </div>
     </Link>
--- src/components/newRecipe/NewRecipe.jsx
+++ src/components/newRecipe/NewRecipe.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import Nav from '../nav/Nav'
 
 export default function NewRecipe() {
+
+
+
+console.log(newRecipe.hhh);
+
+function hey(){
+  console.log(newRecipe.name);
+  console.log("dedwsx");
+
+
+}
+
   return (
+
+
+
     <div className='bgImg '>
         <Nav/>
         <div className='w-100 d-flex justify-content-center'>
         <div className='w-50 flex-column d-flex border mt-5'>
-          <h1>שם המתכוון</h1>
-          <h2>surgirgr</h2>
+          <input type="text" onChange={(e)=>newRecipe.name = e.target.value } />
+          <button onClick={hey()}>change</button>
+          
        </div>
        </div>
     </div>
