f48e7de7385a79bcbf7b67289f1b8814c9031e4a
haim hale <hayim9939@gmail.com>
2023-02-15 20:36:52 +0200

    h

--- src/components/Card/NewCard.jsx
+++ src/components/Card/NewCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function NewCard() {
     return (
      <>
-     <Link to={""}>
+     <Link to={"/newrecipe"}>
         <div className="card text-dark NewCardHover m-3">
           <img className='imgStyle' width={"100%"} height={"100%"} src="https://i.imagesup.co/images2/81b86e9d7be9582a7fd0cbc3bd9fdfdb606acac1.jpg" alt="Newcard" />
           <div className="card-img-overlay ">
