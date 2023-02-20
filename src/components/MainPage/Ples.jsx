d22aba1b3c2c77d57cc1ea5fc6a5d0d1402b55be
moshecc <moshec545@gmail.com>
2023-02-20 13:10:55 +0200

    plus

--- src/components/MainPage/Ples.jsx
+++ src/components/MainPage/Ples.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
-
+import React from "react";
+import Box from "@mui/material/Box";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import PrintIcon from "@mui/icons-material/Print";
+import {
+  WhatsappShareButton,
+  WhatsappIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  EmailShareButton,
+  EmailIcon,
+  TelegramShareButton,
+  TelegramIcon,
+} from "react-share";
+import { useContext } from "react";
+import { ContextData } from "../../App";
 
 export default function Ples() {
+  const { currentOpen } = useContext(ContextData);
 
-    const actions = [
-        { icon: <FileCopyIcon />, name: 'Copy' },
-        { icon: <SaveIcon />, name: 'Save' },
-        { icon: <PrintIcon />, name: 'Print' },
-        { icon: <ShareIcon />, name: 'Share' },
-      ];
-      
   return (
     <>
-    <Box sx={{}} >
-    <SpeedDial ariaLabel="SpeedDial basic example"  icon={<SpeedDialIcon />}>
-      {actions.map((action) => (
-        <SpeedDialAction
-          key={action.name}
-          icon={action.icon}
-          tooltipTitle={action.name}
-        />
-      ))}
-    </SpeedDial>
-  </Box> </> )
-  
+      <Box sx={{}}>
+        <SpeedDial ariaLabel="SpeedDial basic example" icon={<SpeedDialIcon />}>
+          <SpeedDialAction
+            icon={
+              <WhatsappShareButton url={currentOpen}>
+                <WhatsappIcon />
+              </WhatsappShareButton>
+            }
+            tooltipTitle={"שתף"}
+          />
+        </SpeedDial>
+      </Box>
+    </>
+  );
 }
-
