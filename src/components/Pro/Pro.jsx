9161b9b83063cd4cf4304687c0f557cc46572bfc
moshecc <moshec545@gmail.com>
2023-02-28 17:20:03 +0200

    Pro 0.2

--- src/components/Pro/Pro.jsx
+++ src/components/Pro/Pro.jsx
@@ -3,9 +3,9 @@ import "./Pro.css"
 export default function Pro() {
   return (
     <>
-    <div className="d-flex justify-content-center bc-card">
-    <div class="card border" style={{width: "16rem"}}>
-        <div className="bg-secondary d-flex justify-content-center align-items-center"  style={{height: "4rem"}}>Pro</div>
+    <div className="d-flex justify-content-center f">
+    <div class=" border rounded bc-card bc-card mr-2 mt-2" style={{width: "13rem",height: "19rem"}}>
+        <div className="bg-secondary rounded d-flex justify-content-center align-items-center" style={{height: "4rem"}}>Pro</div>
        <div class="card-body">
     <h2 class="d-flex justify-content-center align-items-center">10$</h2>
     <div className="mt-3 mb-3">
@@ -14,7 +14,7 @@ export default function Pro() {
    <span className="d-flex justify-content-center">n</span>
    <span className="d-flex justify-content-center">n</span>
     </div>
-  <button className="col-11">קנה עכשיו</button>
+  <button className="col-12 btn border">קנה עכשיו</button>
   </div>
 </div>
 </div>
