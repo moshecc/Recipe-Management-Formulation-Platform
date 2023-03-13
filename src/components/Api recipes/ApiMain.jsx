8300424b84965d9ed43bdd1ad5e5ec9c648eb6ec
haim hale <hayim9939@gmail.com>
2023-03-13 14:19:24 +0200

    123

--- src/components/Api recipes/ApiMain.jsx
+++ src/components/Api recipes/ApiMain.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function ApiMain() {
+ 
+const apiKode = "bf50efed1f67407f9bb3dcef2eefac27";
+
+useEffect(() => {
+  
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=pizza&apiKey=${apiKode}&number=50`)
+	.then(response => response.json())
+	.then(data =>console.log(data))
+	.catch(err => console.error(err));
+
+}, [])
+
+
+
   return (
     <div>ApiMain</div>
   )
