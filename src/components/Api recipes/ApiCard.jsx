b1a1ffa032aaf3aad4600439bb28950fd9f237a1
moshecc <moshec545@gmail.com>
2023-03-13 12:47:09 +0200

    apiCard

--- src/components/Api recipes/ApiCard.jsx
+++ src/components/Api recipes/ApiCard.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 export default function ApiCard() {
   return (
-    <div>ApiCard</div>
+    <>
+     <Card sx={{ maxWidth: 220 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image=""
+          alt=""
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </>
   )
 }
