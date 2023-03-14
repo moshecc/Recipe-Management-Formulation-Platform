8b1bca1edb290c7d18f89b30346fc8256c1418c1
moshecc <moshec545@gmail.com>
2023-03-14 15:53:37 +0200

    v

--- src/components/Api recipes/ApiMain.jsx
+++ src/components/Api recipes/ApiMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { IoMdReturnRight } from "react-icons/io";
 import { Link } from "react-router-dom";
