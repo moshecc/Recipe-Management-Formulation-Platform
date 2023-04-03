2a0eae537f03e773035e15bfb684ba89e5760cf6
moshecc <moshec545@gmail.com>
2023-04-03 10:21:38 +0300

    aboutus

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./AboutUs.css";
-import "../newRecipe/NewRecipe.css"
+import "../newRecipe/NewRecipe.css";
 import { IoMdReturnRight } from "react-icons/io";
 import { SiFacebook, SiGmail, SiInstagram } from "react-icons/si";
 import { Link } from "react-router-dom";
@@ -11,73 +11,117 @@ export default function AboutUs() {
       <div className="bgImg ">
         <Link to={"/"}>
           <div className="btn back-button">
-            <div><IoMdReturnRight size={30} /></div>
+            <div>
+              <IoMdReturnRight size={30} />
+            </div>
           </div>
         </Link>
         <div className="container-fluid w-100 d-flex justify-content-center">
           <div className="aboutUs mt-5 mb-5 col-9">
             <div>
-              <h1 className="d-flex mt-4 justify-content-center color-bg">קצת עלינו</h1>
-              <p className="d-flex mt-4 justify-content-center text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores aperiam nisi eaque libero ullam impedit laudantium
-                similique quaerat ratione ex quae, inventore at quidem
-                recusandae quibusdam officiis optio? Tempora, soluta.
+              <div className="ml-5 mr-5">
+              <h1 className="d-flex mt-4 justify-content-center color-bg">
+                קצת עלינו
+              </h1>
+              <p className="mt-4 text-center h6 mb-4">
+                אנו סטודנטים ללימודי תוכנה המתמחים בפיתוח תוכנה ועיצוב אתרים. אנו
+                פועלים על פרויקט של אתר לספר מתכונים אישי, שמטרתו לאפשר
+                למשתמשים לאחסן לחפש ולשתף מתכונים שונים בצורה נוחה ומהירה. אנו משתמשים
+                בטכנולוגיות חדשות כדי לפתח אתר מתקדם ומתאים לצרכי המשתמשים. אנו
+                עובדים קשה כדי ליצור מוצר ייחודי ויעיל שיספק את הצרכים של
+                המשתמשים בצורה מקצועית ומהירה. אני מצפה לראות את הפרויקט שלנו
+               .צומח ומתפתח לאתר מוצלח ונגיש לכל משתמש 
+                <br />
+              <span className="chat">.בנתיים עם יש הצעות ליעול ושיפור המערכת אנא צרו קשר *</span>
               </p>
-              {/* <a href="tel:054675119">054675119</a> */}
+              <samp className="chat h5">*_* Chat GPT  נכתב על ידי *_*</samp>
+              </div>
               <div>
                 <div className="d-flex justify-content-center mt-5 about-text">
-                  <h3 className="col-8 color-bg">  פרטים ליצירת קשר</h3>
+                  <h3 className="col-8 color-bg"> פרטים ליצירת קשר</h3>
                 </div>
                 <div className="row d-flex justify-content-center mt-4 mb-4">
                   <div className="card card-style m-1">
                     <div className="card-body ">
                       <div className="d-flex justify-content-center">
-                        <samp className="about-text d-flex fontW align-items-center">צרו איתנו קשר דרך המייל</samp>
+                        <samp className="about-text d-flex fontW align-items-center">
+                          צרו איתנו קשר דרך המייל
+                        </samp>
                         <lord-icon
                           src="https://cdn.lordicon.com/egpbfgcp.json"
                           trigger="hover"
                           colors="primary:#c71f16,secondary:#109121"
-                          style={{ width: "70px", height: "70px" }}>
-                        </lord-icon>
+                          style={{ width: "70px", height: "70px" }}
+                        ></lord-icon>
                       </div>
                       <span className="card-text row d-flex justify-content-center mt-2 nav-link">
                         <SiGmail className="mt-1 mb-2" size={20} color="red" />
-                        <a className="ml-2 myFont linkStyle" href="mailto:myrecipebook23@gmail.com">myrecipebook23@gmail.com</a>
+                        <a
+                          className="ml-2 myFont linkStyle"
+                          href="mailto:myrecipebook23@gmail.com"
+                        >
+                          myrecipebook23@gmail.com
+                        </a>
                       </span>
                     </div>
                   </div>
                   <div className="card card-style m-1">
                     <div className="card-body ">
                       <div className="d-flex justify-content-center">
-                        <samp className="about-text d-flex fontW align-items-center"> עקבו אחרינו בדף האנסטגרם</samp>
+                        <samp className="about-text d-flex fontW align-items-center">
+                          {" "}
+                          עקבו אחרינו בדף האנסטגרם
+                        </samp>
                         <lord-icon
                           src="https://cdn.lordicon.com/ridbdkcb.json"
                           trigger="hover"
                           colors="primary:#e8308c,secondary:#c76f16"
-                          style={{ width: "70px", height: "70px" }}>
-                        </lord-icon>
+                          style={{ width: "70px", height: "70px" }}
+                        ></lord-icon>
                       </div>
                       <span className="card-text row d-flex justify-content-center mt-2">
-                        <SiInstagram className="mt-1 mb-2" size={20} color="#e8308c" />
-                        <a className="ml-2 myFont linkStyle" target={"_blank"} href="https://www.instagram.com/myrecipebook2023/">myrecipebook2023</a>
+                        <SiInstagram
+                          className="mt-1 mb-2"
+                          size={20}
+                          color="#e8308c"
+                        />
+                        <a
+                          className="ml-2 myFont linkStyle"
+                          target={"_blank"}
+                          href="https://www.instagram.com/myrecipebook2023/"
+                        >
+                          myrecipebook2023
+                        </a>
                       </span>
                     </div>
                   </div>
                   <div className="card card-style m-1">
                     <div className="card-body ">
                       <div className="d-flex justify-content-center">
-                        <samp className="about-text d-flex fontW align-items-center"> בקרו בדף שלנו בפייסבוק </samp>
+                        <samp className="about-text d-flex fontW align-items-center">
+                          {" "}
+                          בקרו בדף שלנו בפייסבוק{" "}
+                        </samp>
                         <lord-icon
                           src="https://cdn.lordicon.com/ppezgpha.json"
                           trigger="hover"
                           colors="primary:#2516c7,secondary:#848484"
-                          style={{ width: "70px", height: "70px" }}>
-                        </lord-icon>
+                          style={{ width: "70px", height: "70px" }}
+                        ></lord-icon>
                       </div>
                       <span className="card-text row d-flex justify-content-center mt-2">
-                        <SiFacebook className="mt-1 mb-2" size={20} color="#2516c2" />
-                        <a className="ml-2 myFont linkStyle" target={"_blank"} href="https://www.facebook.com/profile.php?id=100091541614136">MyRecipeBook</a>
+                        <SiFacebook
+                          className="mt-1 mb-2"
+                          size={20}
+                          color="#2516c2"
+                        />
+                        <a
+                          className="ml-2 myFont linkStyle"
+                          target={"_blank"}
+                          href="https://www.facebook.com/profile.php?id=100091541614136"
+                        >
+                          MyRecipeBook
+                        </a>
                       </span>
                     </div>
                   </div>
--- src/components/Pro/Pro.jsx
+++ src/components/Pro/Pro.jsx
@@ -34,43 +34,37 @@ export default function Pro() {
         <b className="d-flex col-8 h2 justify-content-center align-items-center mb-5 mt-5 text-pro">ברוכים הבאים לגרסת הפרמיום</b>
         <div className=" container row justify-content-center ">
 
-          <div class=" mr-2 mb-2 border bc-card bc-card mt-0 mt-lg-5" style={{ width: "16rem", height: "21rem" }}>
+          <div class=" mr-2 mb-2 border bc-card bc-card mt-0 mt-lg-5" style={{ width: "16rem", height: "20rem" }}>
             <b className="bg-pro d-flex justify-content-center align-items-center" style={{ height: "4rem" }}>Basic</b>
             <div class="card-body">
               <h2 class="d-flex justify-content-center align-items-center">₪0</h2>
               <div className="mt-3 mb-3 c">
                 <span className="d-flex h5 justify-content-center">   עד 50 מתכונים *</span>
                 <span className="d-flex h5 justify-content-center">  פרסומות *</span>
-                <span className="d-flex h5 justify-content-center">n</span>
-                <span className="d-flex h5 justify-content-center">n</span>
               </div>
-              <div className="col-12 btn border bg-secondary">Free</div>
+              <div className="col-12 btn border bg-secondary mt-4">Free</div>
             </div>
           </div>
-          <div class="mr-2 mb-2 border bc-card bc-card" style={{ width: "16rem", height: "21rem" }}>
+          <div class="mr-2 mb-2 border bc-card bc-card" style={{ width: "16rem", height: "20rem" }}>
             <b className="bg-pro d-flex justify-content-center align-items-center" style={{ height: "4rem" }}>Chef</b>
             <div class="card-body">
               <h2 class="d-flex justify-content-center align-items-center">₪100</h2>
               <div className="mt-3 mb-3 c">
                 <span className="d-flex h5 justify-content-center">עד 300 מתכונים *</span>
                 <span className="d-flex h5 justify-content-center"> ללא פרסומות *</span>
-                <span className="d-flex h5 justify-content-center">n</span>
-                <span className="d-flex h5 justify-content-center">n</span>
               </div>
-              <div onClick={() =>{ paymant("100.00" , "Chef" ,"עד 300 מתכונים")}} className="col-12 btn border bg-danger shake">קנה עכשיו</div>
+              <div onClick={() =>{ paymant("100.00" , "Chef" ,"עד 300 מתכונים")}} className="col-12 btn border bg-danger shake mt-4">קנה עכשיו</div>
             </div>
           </div>
-          <div class="mr-2 mb-2 border bc-card bc-card mt-0 mt-lg-5" style={{ width: "16rem", height: "21rem" }}>
+          <div class="mr-2 mb-2 border bc-card bc-card mt-0 mt-lg-5" style={{ width: "16rem", height: "20rem" }}>
             <b className="bg-pro d-flex justify-content-center align-items-center" style={{ height: "4rem" }}>Pro</b>
             <div class="card-body">
               <h2 class="d-flex justify-content-center align-items-center">₪50</h2>
               <div className="mt-3 mb-3 c">
                 <span className="d-flex h5 justify-content-center">  עד 150 מתכונים •</span>
                 <span className="d-flex h5 justify-content-center"> ללא פרסומות •</span>
-                <span className="d-flex h5 justify-content-center">n</span>
-                <span className="d-flex h5 justify-content-center">n</span>
               </div>
-              <div onClick={() => {paymant("50.00" , "Pro" ,"עד 150 מתכונים" )}} className="col-12 btn border bg-danger shake">קנה עכשיו</div>
+              <div onClick={() => {paymant("50.00" , "Pro" ,"עד 150 מתכונים" )}} className="col-12 btn border bg-danger shake mt-4">קנה עכשיו</div>
             </div>
 
           </div>
