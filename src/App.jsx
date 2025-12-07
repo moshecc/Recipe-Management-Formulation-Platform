845574fed529c5821d549e7cd1fc870e78f493fc
moshecc <moshec545@gmail.com>
2025-12-07 23:56:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './components/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+     <HomePage/>hhhhhh
+    </div>
+  );
+}kjkgkikkh
+Moshe The King
+export default App;
